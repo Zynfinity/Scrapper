86d9071619958acd7c01f5518e6d0bcd610cdf5c
fajar55 <67202122+fajar55@users.noreply.github.com>
2021-10-17 15:58:05 +0700

    Update scrapper.js

--- scrapper.js
+++ scrapper.js
@@ -7,6 +7,34 @@ const request = require('request');
 const randomarray = async (array) => {
 	return array[Math.floor(Math.random() * array.length)]
 }
+exports.stickersearch = async(query) => {
+	return new Promise((resolve) => {
+		axios.get(`https://getstickerpack.com/stickers?query=${query}`)
+    .then(({data}) => {
+      const $ = cheerio.load(data)
+      const link = [];
+      $('#stickerPacks > div > div:nth-child(3) > div > a').each(function(a, b){
+        link.push($(b).attr('href'))
+      })
+      rand = link[Math.floor(Math.random() * link.length)]
+      axios.get(rand)
+      .then(({data}) => {
+        const $$ = cheerio.load(data)
+        const url = [];
+        $$('#stickerPack > div > div.row > div > img').each(function(a, b){
+          url.push($$(b).attr('src').split('&d=')[0])
+        })
+        resolve({
+          creator: 'Fajar Ihsana',
+          title: $$('#intro > div > div > h1').text(),
+          author: $$('#intro > div > div > h5 > a').text(),
+          author_link: $$('#intro > div > div > h5 > a').attr('href'),
+          sticker: url
+        })
+      })
+    })
+	})
+}
 exports.randomtt = async (query) => {
 	return new Promise((resolve, reject) => {
 		axios.get('https://brainans.com/search?query=' + query)
