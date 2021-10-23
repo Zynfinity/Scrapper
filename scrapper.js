3e8426dbaa80b2f67bdef6c130536ef96764c762
Fajar Ihsana <67202122+Zynfinity@users.noreply.github.com>
2021-10-23 12:47:28 +0700

    Add rexdlsearch + rexdl downloader

--- scrapper.js
+++ scrapper.js
@@ -7,6 +7,87 @@ const request = require('request');
 const randomarray = async (array) => {
 	return array[Math.floor(Math.random() * array.length)]
 }
+exports.rexdl = async (query) => {
+	return new Promise((resolve) => {
+		axios.get('https://rexdl.com/?s=' + query)
+			.then(({
+				data
+			}) => {
+				const $ = cheerio.load(data)
+				const judul = [];
+				const jenis = [];
+				const date = [];
+				const desc = [];
+				const link = [];
+				const thumb = [];
+				const result = [];
+				$('div > div.post-content').each(function(a, b) {
+					judul.push($(b).find('h2.post-title > a').attr('title'))
+					jenis.push($(b).find('p.post-category').text())
+					date.push($(b).find('p.post-date').text())
+					desc.push($(b).find('div.entry.excerpt').text())
+					link.push($(b).find('h2.post-title > a').attr('href'))
+				})
+				$('div > div.post-thumbnail > a > img').each(function(a, b) {
+					thumb.push($(b).attr('data-src'))
+				})
+				for (let i = 0; i < judul.length; i++) {
+					result.push({
+						creator: 'Fajar Ihsana',
+						judul: judul[i],
+						kategori: jenis[i],
+						upload_date: date[i],
+						deskripsi: desc[i],
+						thumb: thumb[i],
+						link: link[i]
+					})
+				}
+				resolve(result)
+			})
+	})
+}
+exports.rexdldown = async (link) => {
+	return new Promise((resolve) => {
+		axios.get(link)
+			.then(({
+				data
+			}) => {
+				const $ = cheerio.load(data)
+				const link = [];
+				const url = [];
+				const link_name = [];
+				const judul = $('#page > div > div > div > section > div:nth-child(2) > article > div > h1.post-title').text();
+				const plink = $('#page > div > div > div > section > div:nth-child(2) > center:nth-child(3) > h2 > span > a').attr('href')
+				axios.get(plink)
+					.then(({
+						data
+					}) => {
+						const $$ = cheerio.load(data)
+						$$('#dlbox > ul.dl > a > li > span').each(function(a, b) {
+							deta = $$(b).text();
+							link_name.push(deta)
+						})
+						$$('#dlbox > ul.dl > a').each(function(a, b) {
+							url.push($$(b).attr('href'))
+						})
+						for (let i = 0; i < link_name.length; i++) {
+							link.push({
+								link_name: link_name[i],
+								url: url[i]
+							})
+						}
+						resolve({
+							creator: 'Fajar Ihsana',
+							judul: judul,
+							update_date: $$('#dlbox > ul.dl-list > li.dl-update > span:nth-child(2)').text(),
+							version: $$('#dlbox > ul.dl-list > li.dl-version > span:nth-child(2)').text(),
+							size: $$('#dlbox > ul.dl-list > li.dl-size > span:nth-child(2)').text(),
+							download: link
+						})
+					})
+			})
+	})
+}
 exports.merdekanews = async () => {
 	return new Promise((resolve) => {
 		axios.get('https://www.merdeka.com/peristiwa/')
@@ -913,43 +994,43 @@ exports.searchgore = async (query) => {
 }
 exports.randomgore = async () => {
 	return new Promise(async (resolve, reject) => {
-			rand = Math.floor(Math.random() * 218) + 1
-			randvid = Math.floor(Math.random() * 16) + 1
-			if (rand === 1) {
-				slink = 'https://seegore.com/gore/'
-			} else {
-				slink = `https://seegore.com/gore/page/${rand}/`
-			}
-			axios.get(slink)
-				.then(({
-					data
-				}) => {
-					const $ = cheerio.load(data)
-					const link = [];
-					const result = [];
-					const username = [];
-					const linkp = $(`#post-items > li:nth-child(${randvid}) > article > div.post-thumbnail > a`).attr('href')
-					const thumbb = $(`#post-items > li:nth-child(${randvid}) > article > div.post-thumbnail > a > div > img`).attr('src')
-					axios.get(linkp)
-						.then(({
-							data
-						}) => {
-							const $$ = cheerio.load(data)
-							const format = {
-								judul: $$('div.single-main-container > div > div.bb-col.col-content > div > div > div > div > header > h1').text(),
-								views: $$('div.single-main-container > div > div.bb-col.col-content > div > div > div > div > div.s-post-meta-block.bb-mb-el > div > div > div.col-r.d-table-cell.col-md-6.col-sm-6.text-right-sm > div > span > span.count').text(),
-								comment: $$('div.single-main-container > div > div.bb-col.col-content > div > div > div > div > div.s-post-meta-block.bb-mb-el > div > div > div.col-r.d-table-cell.col-md-6.col-sm-6.text-right-sm > div > a > span.count').text() == '' ? 'Tidak ada komentar' : $$('div.single-main-container > div > div.bb-col.col-content > div > div > div > div > div.s-post-meta-block.bb-mb-el > div > div > div.col-r.d-table-cell.col-md-6.col-sm-6.text-right-sm > div > a > span.count').text(),
-								thumb: thumbb,
-								link: $$('video > source').attr('src')
-							}
-							const result = {
-								creator: 'Fajar Ihsana',
-								data: format
-							}
-							resolve(result)
-						})
-				.catch(reject)
-		})
+		rand = Math.floor(Math.random() * 218) + 1
+		randvid = Math.floor(Math.random() * 16) + 1
+		if (rand === 1) {
+			slink = 'https://seegore.com/gore/'
+		} else {
+			slink = `https://seegore.com/gore/page/${rand}/`
+		}
+		axios.get(slink)
+			.then(({
+				data
+			}) => {
+				const $ = cheerio.load(data)
+				const link = [];
+				const result = [];
+				const username = [];
+				const linkp = $(`#post-items > li:nth-child(${randvid}) > article > div.post-thumbnail > a`).attr('href')
+				const thumbb = $(`#post-items > li:nth-child(${randvid}) > article > div.post-thumbnail > a > div > img`).attr('src')
+				axios.get(linkp)
+					.then(({
+						data
+					}) => {
+						const $$ = cheerio.load(data)
+						const format = {
+							judul: $$('div.single-main-container > div > div.bb-col.col-content > div > div > div > div > header > h1').text(),
+							views: $$('div.single-main-container > div > div.bb-col.col-content > div > div > div > div > div.s-post-meta-block.bb-mb-el > div > div > div.col-r.d-table-cell.col-md-6.col-sm-6.text-right-sm > div > span > span.count').text(),
+							comment: $$('div.single-main-container > div > div.bb-col.col-content > div > div > div > div > div.s-post-meta-block.bb-mb-el > div > div > div.col-r.d-table-cell.col-md-6.col-sm-6.text-right-sm > div > a > span.count').text() == '' ? 'Tidak ada komentar' : $$('div.single-main-container > div > div.bb-col.col-content > div > div > div > div > div.s-post-meta-block.bb-mb-el > div > div > div.col-r.d-table-cell.col-md-6.col-sm-6.text-right-sm > div > a > span.count').text(),
+							thumb: thumbb,
+							link: $$('video > source').attr('src')
+						}
+						const result = {
+							creator: 'Fajar Ihsana',
+							data: format
+						}
+						resolve(result)
+					})
+					.catch(reject)
+			})
 	})
 }
 exports.textmakervid = async (text1, style) => {
